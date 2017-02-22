d4435d9df8624daef33344e389148be6d7c02840
Phil Porada <philporada@gmail.com>
2017-02-21 21:34:39 -0500

    Ran rubocop

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -1,18 +1,17 @@
 # encoding: utf-8
-#require "logstash/devutils/rspec/spec_helper"
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/plugin'
+require 'logstash/filters/grok'
 
-require "logstash/devutils/rspec/spec_helper"
-require "logstash/plugin"
-
-describe "simple syslog line" do
-
-  file = File.open("/test/conf/filter.conf","rb")
+describe 'simple syslog line' do
+  file = File.open('/test/conf/filter.conf', 'rb')
   config file.read
   file.close
 
-  sample "Mar 16 00:01:25 evita postfix/smtpd[1713]: connect from tcnksm.net[168.100.1.3]" do
-    insist { subject["message"] } == "connect from tcnksm.net[168.100.1.3]"
-    insist { subject["program"] } == "postfix/smtpd"
+  message = %(Mar 16 00:01:25 evita postfix/smtpd[1713]: connect from tcnksm.net[168.100.1.3])
+
+  sample('@message' => message) do
+    insist { subject['message'] }.should be 'connect from tcnksm.net[168.100.1.3]'
+    insist { subject['program'] }.should be 'postfix/smtpd'
   end
 end
-
